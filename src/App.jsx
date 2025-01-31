9438bf0e892873df6366ac3885b81b5cd5c9f66b
akshit-codes <chhabra.akshit@gmail.com>
2025-01-31 07:27:27 +0530

    added events page stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { Routes, Route } from "react-router-dom";
-import "./App.css";
-import React from "react";
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import React from 'react';
 import Home from './pages/home.jsx';
 import About from './pages/about.jsx';
+import EventsPage from './pages/events.jsx';
+import EventPage from './pages/event.jsx';
+
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="about" element={<About />} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="about" element={<About />} />
+			<Route path="events" element={<EventsPage />} />
+			<Route path="event" element={<EventPage />} />
+		</Routes>
+	);
 }
 
 export default App;
