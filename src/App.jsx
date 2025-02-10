29c1c02d5ca43af62bf999b650266c2e6067dd74
yuvraj-rathod-1202 <rathodyuvraj12022007>
2025-02-11 01:56:21 +0530

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import React from "react";
 import Home from './pages/home.jsx';
 import About from './pages/about.jsx';
 import Rewards from "./pages/Rewards.jsx";
+import Navbar from "./components/Navbar.jsx";
 function App() {
   return (
+    <div>
+    <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="about" element={<About />} />
       <Route path="rewards" element={<Rewards />} />
     </Routes>
+    </div>
   );
 }
 
