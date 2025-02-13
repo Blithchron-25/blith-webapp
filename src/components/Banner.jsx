6554a9d078edf51299a7dffd57a2feec565b050b
Romit Mohane <69901221+Reckadon@users.noreply.github.com>
2025-02-13 20:36:30 +0530

    banner changes and pwa updatyes

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import './Banner.css';
+import "./Banner.css";
 
 const Banner = () => {
-  return (
-    <div className="banner">
-      <div className="banner-content">
-        <div className="banner-message">
-          <p>Please open this application on a mobile phone for the best experience.</p>
-        </div>
-        <div className="banner-image">
-          <img src="/404-error.jpg" alt="Use mobile" />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="banner">
+			<div className="banner-content">
+				<div className="banner-message">
+					<div>
+						<h2>Blithchron 2025</h2>
+						<p>Please open this website on a mobile phone for using the app.</p>
+					</div>
+				</div>
+				<div className="banner-image">
+					<img src="/404-error.jpg" alt="Use mobile" />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Banner;
